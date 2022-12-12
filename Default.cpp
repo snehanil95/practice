b54308be023f4b033e7437f2dd12b7d5c100854a
sneha Anil Memane <114849578+snehanil95@users.noreply.github.com>
2022-12-12 22:31:11 +0530

    Add files via upload

---
+++ Default.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+class Demo
+{
+    public:
+        int i,j;
+        
+        // Parametrised constructor with DEFAULT values
+        Demo(int x = 10, int y = 20)
+        {
+            i = x;
+            j = y;
+            cout<<"value of x=%d"<<X;
+            cout<<"value of y=%d"<<y;
+        }
+};
+int main()
+{
+    Demo obj1;          // 10   20
+    Demo obj2(11);      // 11   20
+    Demo obj3(11,21);   // 11   21
+
+    return 0;
+}
